18e5087e040f510f87452122d0dbec586072520f
wbrunovieira <wbrunovieira77@gmail.com>
2020-09-02 16:27:17 +0100

    Encapsulation practice

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+
+        }
+
+        private double balance = 0;
+
+        //Define a method named Deposit that will accept a
+        //double and store that value in the balance field
+
+        //Encapsulation 
+        public void Deposit(double amount)
+        {
+
+            balance = amount;
+
+        }
+
+        //Define a method named GetBalance that
+        //will return the amount stored in the balance field
+
+        public String GetBalance()
+        {
+            return$"${balance}";
+        }
+
+
+
+    }
+
+}
+
+
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         BankAccount brunoAccount = new BankAccount();
+
+            Console.WriteLine("How much would loke to deposit? ");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            brunoAccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you! Your account balance is now: {brunoAccount.GetBalance()}");
+
+
+
+        }
+    }
+}
